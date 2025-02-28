706faf7cba4c60fb420bd418b33ea54c222ff5d2
Chanella Keza <chanella.keza@stu.ocsb.ca>
2025-02-28 16:52:43 +0000

    Added area and perimeter.py

---
+++ area and perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: chanella
+# Date: feb 27, 2025
+# This program calculates the area and perimeter of a rectangle
+# with dimensions 4cm X 2cm.
+
+
+def main():
+    # This function calculates the area and perimeter
+    print("If a rectangle has dimensions")
+    print("4cm x 2cm")
+    print("")
+    print("Area is{}cm^2".format(4 * 2))
+    print("perimeter is {}cm^2".format(4 * 2))
+
+
+if __name__ == "__main__":
+    main()
